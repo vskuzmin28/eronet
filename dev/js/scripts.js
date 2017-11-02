67e39f8d686601e44a284ed6daa5f56b07bbd737
vskuzmin28 <vskuzmin28@gmail.com>
2017-11-02 12:57:19 +0300

    add block action

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -27,47 +27,39 @@ $('.send-form').submit(function() {
   })
 
 
-$(function () {
-
-  $('#new-banner').responsiveSlides({
-    auto: true,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "im-new-banners"
-  });
-
-  $('#main-banner').responsiveSlides({
-    auto: true,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "main-banners"
-  });
-
-  $('#team-banner').responsiveSlides({
-    auto: true,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "team-banners"
-  });
-
-  $('#reviews-banner').responsiveSlides({
-    auto: false,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "reviews-banners"
-  });
-
-  $('#meetings-banner').responsiveSlides({
-    auto: false,
-    pager: false,
-    nav: true,
-    speed: 500,
-    namespace: "meetings-banners"
-  });
-
-
-});
+function countDown(second,endMinute,endHour,endDay,endMonth,endYear) {
+var now = new Date();
+second = (arguments.length == 1) ? second + now.getSeconds() : second;
+endYear =  typeof(endYear) != 'undefined' ?  endYear : now.getFullYear();            
+endMonth = endMonth ? endMonth - 1 : now.getMonth();  //номер месяца начинается с 0   
+endDay = typeof(endDay) != 'undefined' ? endDay :  now.getDate();    
+endHour = typeof(endHour) != 'undefined' ?  endHour : now.getHours();
+endMinute = typeof(endMinute) != 'undefined' ? endMinute : now.getMinutes();   
+//добавляем секунду к конечной дате (таймер показывает время уже спустя 1с.) 
+var endDate = new Date(endYear,endMonth,endDay,endHour,endMinute,second+1); 
+var interval = setInterval(function() { //запускаем таймер с интервалом 1 секунду
+    var time = endDate.getTime() - now.getTime();
+    if (time < 0) {                      //если конечная дата меньше текущей
+        clearInterval(interval);
+        alert("Неверная дата!");            
+    } else {            
+        var days = Math.floor(time / 864e5);
+        var hours = Math.floor(time / 36e5) % 24; 
+        var minutes = Math.floor(time / 6e4) % 60;
+        var seconds = Math.floor(time / 1e3) % 60;  
+        var digit='<div style="width:70px;float:left;text-align:center">'+
+        '<div style="font-family:Stencil;font-size:65px;">';
+        var text='</div><div>'
+        var end='</div></div><div style="float:left;font-size:45px;">:</div>'
+        document.getElementById('mytimer').innerHTML = '<div>осталось: </div>'+
+        digit+days+text+'Дней'+end+digit+hours+text+'Часов'+end+
+        digit+minutes+text+'Минут'+end+digit+seconds+text+'Секунд';
+        if (!seconds && !minutes && !days && !hours) {              
+            clearInterval(interval);
+            alert("Время вышло!");              
+        }           
+    }
+    now.setSeconds(now.getSeconds() + 1); //увеличиваем текущее время на 1 секунду
+}, 1000);
+}
+countDown(30); //устанавливаем таймер на 30 секунд  
